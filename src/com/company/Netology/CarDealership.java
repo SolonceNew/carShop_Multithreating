fcf1a68fe8f73aecc0b9d9327df0a4598a06f951
Anna Fabrichnaya <fabrichnaya.anna@bk.ru>
2021-11-29 17:56:35 +0300

    lock carShop

--- src/com/company/Netology/CarDealership.java
+++ src/com/company/Netology/CarDealership.java
@@ -2,25 +2,34 @@ package com.company.Netology;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
 
 public class CarDealership {
 
     private final static int BUY_TIME = 1000;
     private final static int PRODUCE_TIME = 2000;
     final List<Car> cars = new ArrayList<>();
+    private final Lock lock = new ReentrantLock();
+    private final Condition condition = lock.newCondition();
 
     public synchronized void buyCar() {
         try {
+            lock.lock();
             System.out.println(Thread.currentThread().getName() + " зашел в автосалон");
             while(cars.size() == 0) {
                 System.out.println("Машин нет!");
-                wait();
+                condition.await();
             }
             Thread.sleep(BUY_TIME);
             System.out.println(Thread.currentThread().getName() + " уехал на новеньком авто");
             cars.remove(0);
         } catch (InterruptedException e) {
             e.printStackTrace();
+        } finally {
+            lock.unlock();
         }
 
     }
@@ -30,11 +39,12 @@ public class CarDealership {
             Thread.sleep(PRODUCE_TIME);
             cars.add(new Car());
             System.out.println(Thread.currentThread().getName() + " выпустил 1 авто");
-            synchronized (this) {
-                notify();
-            }
+            lock.lock();
+            condition.signal();
         } catch (InterruptedException e) {
                 e.printStackTrace();
+        } finally {
+            lock.unlock();
         }
     }
 }
